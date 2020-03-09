dc3cf0dd56378a2bebbe05c5ed28af2196ae0c2d
Dave Sperling <dsperling@smithmicro.com>
2020-03-09 08:38:00 -0700

    Added android-30 (R), minor simplifications

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# Android 30 "R"
+docker build 30 -t smithmicro/android-build:30
+docker build 30/ndk-21 -t smithmicro/android-build:30-ndk-21
+
 # Android 29
 docker build 29 -t smithmicro/android-build:29
 docker build 29/ndk-21 -t smithmicro/android-build:29-ndk-21

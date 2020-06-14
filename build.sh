2efe9b25284ab13a830157b7833e7197ded3171c
Dave Sperling <dsperling@smithmicro.com>
2020-06-14 07:08:26 -0700

    Android 30 Release

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# Android 30
+docker build 30 -t smithmicro/android-build:30
+docker build 30/ndk21 -t smithmicro/android-build:30-ndk21
+
 # Android 29
 docker build 29 -t smithmicro/android-build:29
 docker build 29/ndk21 -t smithmicro/android-build:29-ndk21
@@ -11,7 +15,3 @@ docker build 29/ndk20 -t smithmicro/android-build:29-ndk20
 docker build 28 -t smithmicro/android-build:28
 docker build 28/ndk21 -t smithmicro/android-build:28-ndk21
 docker build 28/ndk20 -t smithmicro/android-build:28-ndk20
-
-# Beta Versions - Android 30 "R"
-docker build 30 -t smithmicro/android-build:30
-docker build 30/ndk21 -t smithmicro/android-build:30-ndk21
